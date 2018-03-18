c057d8ffb6657209df78afe33e69be3d2279483a
lucyL <lucylaurentino@gmail.com>
2018-03-19 11:06:12 +1300

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace dateTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myBirthday = new DateTime(1982, 10, 23); //create instance just
+            Console.WriteLine(myBirthday.ToString("dd MMMM yyyy"));
+
+            var currentDate = DateTime.Now;
+            Console.WriteLine(currentDate.ToString("dd MMMM yyyy"));
+        }
+    }
+}
