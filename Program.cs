9b6672136172ddb15a7ee81e86aa9fdd03645a02
lucyL <lucylaurentino@gmail.com>
2018-03-19 11:53:26 +1300

    more commit

--- Program.cs
+++ Program.cs
@@ -5,12 +5,30 @@ namespace dateTime
     class Program
     {
         static void Main(string[] args)
+        
         {
-            var myBirthday = new DateTime(1982, 10, 23); //create instance just
-            Console.WriteLine(myBirthday.ToString("dd MMMM yyyy"));
+            var myBirthday = new DateTime(1982, 10, 23); //create instance and pass parameters, use new
+            //Console.WriteLine(myBirthday.ToString("dd MMMM yyyy"));
 
-            var currentDate = DateTime.Now;
-            Console.WriteLine(currentDate.ToString("dd MMMM yyyy"));
-        }
-    }
-}
+            var currentDate = DateTime.Now;//static property
+            //Console.WriteLine(currentDate.ToString("dd MMMM yyyy"));
+
+            //Console.WriteLine(myBirthday.DayOfYear);
+            //Console.WriteLine(currentDate.DayOfYear);
+
+            if (myBirthday.DayOfYear < currentDate.DayOfYear){
+                Console.WriteLine("My birthday has already been this year");
+
+            }
+            else{
+                Console.WriteLine("woohoo the best day of the year is still to come...");
+            }
+            //Console.WriteLine(currentDate.Year - myBirthday.Year);
+
+       /* } var age = currentDate.Year - myBirthday.Year;
+
+        if (myBirthday.DayOfYear > currentDate.DayOfYear){
+            age -=1;
+        
+    } Console.WriteLine($"My age is {age}");
+}*/
